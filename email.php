00f8c7de30adc11c31d5a2cdc30290580ebf0b65
Stephen Jochen <stephenjochen2@gmail.com>
2019-09-19 16:34:22 -0400

    added the portfolio to the repo, need to fix the contact form

---
+++ email.php
@@ -0,0 +1,82 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+
+
+    $email_from = 'stephenjochen@gmail.com';
+    $email_subject = "Message from portfolio";
+    $email_body = "You have received a new message from the user $name.\n".
+        "Here is the message:\n $message".
+
+
+  $to = "stephenjochen@gmail.com";
+
+  $headers = "From: $email_from \r\n";
+
+  $headers .= "Reply-To: $visitor_email \r\n";
+
+  mail($to,$email_subject,$email_body,$headers);
+
+  header("Location: index.html");
+
+ ?>
+
+<?php
+
+function IsInjected($str)
+{
+
+    $injections = array('(\n+)',
+           '(\r+)',
+
+           '(\t+)',
+
+           '(%0A+)',
+
+           '(%0D+)',
+
+           '(%08+)',
+
+           '(%09+)'
+
+           );
+
+    $inject = join('|', $injections);
+
+    $inject = "/$inject/i";
+
+     
+
+    if(preg_match($inject,$str))
+
+    {
+
+      return true;
+
+    }
+    else
+
+    {
+
+      return false;
+
+    }
+
+}
+
+
+if(IsInjected($visitor_email))
+
+{
+
+    echo "Bad email value!";
+
+    exit;
+
+}
+
+?>
+
+
